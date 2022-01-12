ae6097c85150de8f46b0b0f7a25cb705eb54e002
RobotSpaceBear <47452069+RobotSpaceBear@users.noreply.github.com>
2022-01-12 18:08:08 +0100

    Removed Shared project, was not used

--- AutoDownloadEnergySaver.Shared/ADES_PowerHelper.cs
+++ AutoDownloadEnergySaver.Shared/ADES_PowerHelper.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace AutoDownloadEnergySaver.Shared
 {
-    internal class ADES_PowerHelper
+    public class ADES_PowerHelper
     {
         public void HibernatePc(int seconds)
         {
